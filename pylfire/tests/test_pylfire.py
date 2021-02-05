79109abd82a58b0217d69172116f2d1216a8ec0b
ulpu <ulpu.remes@aalto.fi>
2021-02-05 17:25:30 +0100

    add precomputed models save/load test

--- pylfire/tests/test_pylfire.py
+++ pylfire/tests/test_pylfire.py
@@ -1,6 +1,7 @@
 import pytest  # noqa: F401; pylint: disable=unused-variable
 
 import numpy as np
+import tempfile
 
 import pylfire
 from pylfire.models import arch
@@ -40,3 +41,30 @@ def test_pylfire():
     lfire_results = lfire_method.infer()
     assert isinstance(lfire_results, ParameterInferenceResult) \
         and isinstance(lfire_results, pylfire.LFIREPosterior)
+
+def test_precomputed_models():
+    """Tests consistency with precomputed models. """
+    arch_model = arch.get_model()
+    params_grid = _create_grid(3)
+
+    # 1: run LFIRE and save models
+
+    lfire_method = pylfire.LFIRE(arch_model, params_grid, batch_size=10)
+    lfire_results = lfire_method.infer()
+
+    temp_file = tempfile.TemporaryFile()
+    lfire_method.save_models(temp_file)
+    temp_file.seek(0)  # simulate closing and opening the file
+
+    # 2: run LFIRE with the saved models
+
+    new_lfire_method = pylfire.LFIRE(arch_model, params_grid, batch_size=10,
+                                     precomputed_models = temp_file)
+    # check that precomputed models were loaded
+    assert new_lfire_method.state['n_batches'] == params_grid.shape[0]
+    new_results = new_lfire_method.infer()
+    # check that results are consistent
+    assert np.all(np.isclose(new_results.posterior_means_array, lfire_results.posterior_means_array))
+    assert np.all(np.isclose(new_results.map_estimates_array, lfire_results.map_estimates_array))
+
+    temp_file.close()
